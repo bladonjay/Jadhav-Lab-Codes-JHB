9fc7ed60a888609eb8100a6c86bd21d336aa15d1
bladonjay <bladonjay@gmail.com>
2022-11-11 16:10:35 -0500

    just path updating, continue cleaning code repo

--- BetaOdorProject/clairesFunctions/jhb_setBetaOderpaths.m
+++ BetaOdorProject/clairesFunctions/jhb_setBetaOderpaths.m
@@ -1,18 +1,30 @@
 % jhb_setBetaOdorPaths
+try
+    allfolders=dir('C:\Users\Jadhavlab\Documents\gitRepos\Jadhav-Lab-Codes\BetaOdorProject\clairesFunctions');
+    allfolders(1:2)=[];
+    allfolders=allfolders([allfolders.isdir]==1);
+    allfolders=allfolders(~contains({allfolders.name},'OLD'));
 
-allfolders=dir('C:\Users\Jadhavlab\Documents\gitRepos\Jadhav-Lab-Codes\BetaOdorProject\clairesFunctions');
-allfolders(1:2)=[];
-allfolders=allfolders([allfolders.isdir]==1);
-allfolders=allfolders(~contains({allfolders.name},'OLD'));
+    addpath('C:\Users\Jadhavlab\Documents\gitRepos\Jadhav-Lab-Codes\BetaOdorProject\clairesFunctions');
 
-addpath('C:\Users\Jadhavlab\Documents\gitRepos\Jadhav-Lab-Codes\BetaOdorProject\clairesFunctions');
+    for i=1:length(allfolders)
+        addpath(genpath(fullfile(allfolders(i).folder,allfolders(i).name)));
+    end
 
-for i=1:length(allfolders)
-    addpath(genpath(fullfile(allfolders(i).folder,allfolders(i).name)));
+catch
+
+
+    allfolders=dir('E:\GithubCodeRepositories\Jadhav-Lab-Codes\BetaOdorProject\clairesFunctions');
+    allfolders(1:2)=[];
+    allfolders=allfolders([allfolders.isdir]==1);
+    allfolders=allfolders(~contains({allfolders.name},'OLD'));
+
+    addpath('E:\GithubCodeRepositories\Jadhav-Lab-Codes\BetaOdorProject\clairesFunctions');
+
+    for i=1:length(allfolders)
+        addpath(genpath(fullfile(allfolders(i).folder,allfolders(i).name)));
+    end
+
 end
-
-
-
-
 
 
