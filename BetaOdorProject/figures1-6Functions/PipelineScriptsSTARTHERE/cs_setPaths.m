98b33f130d2468e0200d377ed4e1d76f9b6b577f
bladonjay <bladonjay@gmail.com>
2022-12-08 09:18:26 -0500

    updating some files, added createcellinfostruct

--- BetaOdorProject/figures1-6Functions/PipelineScriptsSTARTHERE/cs_setPaths.m
+++ BetaOdorProject/figures1-6Functions/PipelineScriptsSTARTHERE/cs_setPaths.m
@@ -7,13 +7,17 @@ end
 topDir=[];
 figDir=[];
 for i=1:26 %nletters in alphabet
-    if exist(sprintf('%s:\\Data\\OdorPlaceAssociation\\',char(driveLetter))) == 7
+    if exist(sprintf('%s:\\Data\\OdorPlaceAssociation\\',char(driveLetter)),'dir') == 7
         topDir = sprintf('%s:\\Data\\OdorPlaceAssociation\\',char(driveLetter));
         figDir = sprintf('%s:\\Figures\\',char(driveLetter));
         return
-    elseif exist(sprintf('%s:\\Brandeis datasets\\OdorPlaceAssociation\\',char(driveLetter))) == 7
+    elseif exist(sprintf('%s:\\Brandeis datasets\\OdorPlaceAssociation\\',char(driveLetter)),'dir') == 7
         topDir = sprintf('%s:\\Brandeis datasets\\OdorPlaceAssociation\\',char(driveLetter));
         figDir = sprintf('%s:\\Brandeis datasets\\OdorPlaceAssociation\\Figures\\',char(driveLetter));
+        return
+    elseif exist(sprintf('%s:\\BrandeisData\\SymanskiData\\',char(driveLetter)),'dir') == 7
+        topDir = sprintf('%s:\\BrandeisData\\SymanskiData\\',char(driveLetter));
+        figDir = sprintf('%s:\\BrandeisData\\SymanskiData\\',char(driveLetter));
         return
     else
         driveLetter=driveLetter+1;
--- BetaOdorProject/figures1-6Functions/PipelineScriptsSTARTHERE/jhb_setBetaOderpaths.m
+++ BetaOdorProject/figures1-6Functions/PipelineScriptsSTARTHERE/jhb_setBetaOderpaths.m
@@ -1,11 +1,11 @@
 % jhb_setBetaOdorPaths
 try
-    allfolders=dir('C:\Users\Jadhavlab\Documents\gitRepos\Jadhav-Lab-Codes\BetaOdorProject\figures1-6Functions');
+    allfolders=dir('C:\\Users\\Jadhavlab\\Documents\\gitRepos\\Jadhav-Lab-Codes\\BetaOdorProject\\figures1-6Functions');
     allfolders(1:2)=[];
     allfolders=allfolders([allfolders.isdir]==1);
     allfolders=allfolders(~contains({allfolders.name},'OLD'));
 
-    addpath('C:\Users\Jadhavlab\Documents\gitRepos\Jadhav-Lab-Codes\BetaOdorProject\figures1-6Functions');
+    addpath('C:\\Users\\Jadhavlab\\Documents\\gitRepos\\Jadhav-Lab-Codes\\BetaOdorProject\\figures1-6Functions');
 
     for i=1:length(allfolders)
         addpath(genpath(fullfile(allfolders(i).folder,allfolders(i).name)));
