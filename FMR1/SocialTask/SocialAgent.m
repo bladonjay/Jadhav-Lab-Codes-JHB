59c26226b2160f7496f7a68e6778fd03ad8b092d
Jay Bladon <bladonjay@gmail.com>
2022-12-15 09:58:01 -0500

    Create SocialAgent.m

---
+++ FMR1/SocialTask/SocialAgent.m
@@ -0,0 +1,13 @@
+% SocialAgent
+% this script generates a fake rat 'agent' that can perform the task and
+% will create useful surrogate datasets witht he same biases that the real
+% rats do
+
+%{
+the biases I want to capture:
+- arm bias
+- arm transition bias (should also capture arm bias)
+- dwell times at each arm
+%}
+
+
